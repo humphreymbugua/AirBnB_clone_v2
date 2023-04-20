ae643478b9d135237ae33e3513a27b15f76ba00c
humphreymbugua <113668644+humphreymbugua@users.noreply.github.com>
2023-04-20 03:58:35 +0300

    AirBnB clone - MySQL.||

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,33 +6,20 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html
+# Check if directory current exist
+if [ -d "/data/web_static/current" ]
+then
+        sudo rm -rf /data/web_static/current
+fi
+# Create a symbolic link to test
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Change ownership to user ubuntu
+chown -hR ubuntu:ubuntu /data
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+# Configure nginx to serve content pointed to by symbolic link to hbnb_static
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
+# Restart server
 service nginx restart
